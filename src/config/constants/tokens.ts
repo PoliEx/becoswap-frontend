aca797718cd0f4f7a76fb806046ea062ccadbd18
PoliEx <85815702+PoliEx@users.noreply.github.com>
2021-06-13 11:20:12 +0700

    Update tokens.ts
    Update NODE

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -1,40 +1,40 @@
 const tokens = {
   bnb: {
-    symbol: 'BNB',
-    projectLink: 'https://www.binance.com/',
+    symbol: 'MATIC',
+    projectLink: 'https://polygon.technology/',
   },
-  beco: {
-    symbol: 'BECO',
+  polex: {
+    symbol: 'POLEX',
     address: {
-      56: '0x8fe4d28476cdd43d36a12eb47dc3243c1925f263',
-      97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+      56: '0xdf7f30243d0e9a070163cb0f092dce9135472fc5',
+      80001: '',
     },
     decimals: 18,
-    projectLink: 'https://becoswap.com/',
+    projectLink: 'https://polyswap.netlify.app/',
   },
   btcb: {
     symbol: 'BTCB',
     address: {
-      56: '0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c',
-      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
+      137: '0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c',
+      80001: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
     },
     decimals: 18,
     projectLink: 'https://bitcoin.org/',
   },
-  wbnb: {
-    symbol: 'wBNB',
+  wmatic: {
+    symbol: 'wMatic',
     address: {
-      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
-      97: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
+      137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+      80001: '',
     },
     decimals: 18,
-    projectLink: 'https://pancakeswap.finance/',
+    projectLink: 'https://polygon.technology/',
   },
   busd: {
     symbol: 'BUSD',
     address: {
-      56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-      97: '',
+      137: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'https://www.paxos.com/busd/',
@@ -42,8 +42,8 @@ const tokens = {
   eth: {
     symbol: 'ETH',
     address: {
-      56: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
-      97: '',
+      137: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'https://ethereum.org/en/',
@@ -51,8 +51,8 @@ const tokens = {
   usdc: {
     symbol: 'USDC',
     address: {
-      56: '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d',
-      97: '',
+      137: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'https://www.centre.io/usdc',
@@ -60,8 +60,8 @@ const tokens = {
   cake: {
     symbol: 'CAKE',
     address: {
-      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-      97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+      137: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'https://pancakeswap.finance/',
@@ -69,8 +69,8 @@ const tokens = {
   usdt: {
     symbol: 'USDT',
     address: {
-      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
-      56: '0x55d398326f99059ff775485246999027b3197955',
+      137: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'https://tether.to/',
@@ -79,8 +79,8 @@ const tokens = {
   tusd: {
     symbol: 'TUSD',
     address: {
-      56: '0x14016e85a25aeb13065688cafb43044c2ef86784',
-      97: '',
+      137: '0x14016e85a25aeb13065688cafb43044c2ef86784',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'https://www.trueusd.com/',
@@ -88,8 +88,8 @@ const tokens = {
   ust: {
     symbol: 'UST',
     address: {
-      56: '0x23396cf899ca06c4472205fc903bdb4de249d6fc',
-      97: '',
+      137: '0x23396cf899ca06c4472205fc903bdb4de249d6fc',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'https://mirror.finance/',
@@ -97,8 +97,8 @@ const tokens = {
   dai: {
     symbol: 'DAI',
     address: {
-      56: '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3',
-      97: '',
+      137: '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'http://www.makerdao.com/',
@@ -107,8 +107,8 @@ const tokens = {
   ltc: {
     symbol: 'LTC',
     address: {
-      56: '0x4338665cbb7b2485a8855a139b75d5e34ab0db94',
-      97: '',
+      137: '0x4338665cbb7b2485a8855a139b75d5e34ab0db94',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'https://litecoin.org/',
@@ -116,8 +116,8 @@ const tokens = {
   xrp: {
     symbol: 'XRP',
     address: {
-      56: '0x1d2f0da169ceb9fc7b3144628db156f3f6c60dbe',
-      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
+      137: '0x1d2f0da169ceb9fc7b3144628db156f3f6c60dbe',
+      80001: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
     },
     decimals: 18,
     projectLink: 'https://ripple.com/xrp/',
@@ -125,8 +125,8 @@ const tokens = {
   uni: {
     symbol: 'UNI',
     address: {
-      56: '0xbf5140a22578168fd562dccf235e5d43a02ce9b1',
-      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
+      137: '0xbf5140a22578168fd562dccf235e5d43a02ce9b1',
+      80001: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
     },
     decimals: 18,
     projectLink: 'https://uniswap.org/',
@@ -134,8 +134,8 @@ const tokens = {
   vai: {
     symbol: 'VAI',
     address: {
-      56: '0x4bd17003473389a42daf6a0a729f6fdb328bbbd7',
-      97: '',
+      137: '0x4bd17003473389a42daf6a0a729f6fdb328bbbd7',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'https://venus.io/',
@@ -143,8 +143,8 @@ const tokens = {
   doge: {
     symbol: 'DOGE',
     address: {
-      56: '0xba2ae424d960c26247dd6c32edc70b295c744c43',
-      97: '',
+      137: '0xba2ae424d960c26247dd6c32edc70b295c744c43',
+      80001: '',
     },
     decimals: 8,
     projectLink: 'https://dogecoin.com/',
@@ -152,8 +152,8 @@ const tokens = {
   trx: {
     symbol: 'TRX',
     address: {
-      56: '0x85eac5ac2f758618dfa09bdbe0cf174e7d574d5b',
-      97: '',
+      137: '0x85eac5ac2f758618dfa09bdbe0cf174e7d574d5b',
+      80001: '',
     },
     decimals: 18,
     projectLink: 'https://tron.network/',
