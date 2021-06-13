91cbb24d5f089c67213936ce8d01a11a8d83afdc
PoliEx <85815702+PoliEx@users.noreply.github.com>
2021-06-13 11:31:16 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -4,10 +4,10 @@ import { FarmConfig } from './types'
 const farms: FarmConfig[] = [
   {
     pid: 1,
-    lpSymbol: 'POLEX-BNB LP',
+    lpSymbol: 'POLEX-MATIC LP',
     lpAddresses: {
-      80001: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      137: '0x93bA7cA08fcB20d0a3381052B8c24bdFb0a9fec7',
+      80001: '',
+      137: '0xeAdbf646bAbca32a2E2a669Cd168f9559fDb0B36',
     },
     token: tokens.polex,
     quoteToken: tokens.wmatic,
@@ -24,22 +24,22 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 4,
-    lpSymbol: 'BNB-USDT LP',
+    lpSymbol: 'MATIC-USDC LP',
     lpAddresses: {
-      80001: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      137: '0x587d26D8235C24DBA51CF4E6629d5327CF43Ae4C',
+      80001: '',
+      137: '0x6e7a5FAFcec6BB1e78bAE2A1F0B612012BF14827',
     },
-    token: tokens.usdt,
+    token: tokens.usdc,
     quoteToken: tokens.wmatic,
   },
   {
     pid: 6,
-    lpSymbol: 'ETH-USDT LP',
+    lpSymbol: 'ETH-USDC LP',
     lpAddresses: {
-      80001: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      137: '0x4C601D261710deA5703F48854AeA2788CE58b9B3',
+      80001: '',
+      137: '0x853Ee4b2A13f8a742d64C8F088bE7bA2131f670d',
     },
-    token: tokens.usdt,
+    token: tokens.usdc,
     quoteToken: tokens.eth,
   },
   
