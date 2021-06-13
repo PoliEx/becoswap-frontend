c1f587701c477d529280fba178ff1859e8430ec9
PoliEx <85815702+PoliEx@users.noreply.github.com>
2021-06-13 11:32:44 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -4,15 +4,15 @@ import { PoolConfig, PoolCategory } from './types'
 const pools: PoolConfig[] = [
   {
     sousId: 0,
-    stakingToken: tokens.beco,
-    earningToken: tokens.beco,
+    stakingToken: tokens.polex,
+    earningToken: tokens.polex,
     contractAddress: {
-      97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
-      56: '0x2aE8E3166fF75aF8aBB9bea2D453F084cBcd8FDA',
+      80001: '',
+      137: '0x0f733c81ff79be10e722eaabf98d55230fed7504',
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '12',
+    tokenPerBlock: '20',
     sortOrder: 1,
     isFinished: false,
   },
