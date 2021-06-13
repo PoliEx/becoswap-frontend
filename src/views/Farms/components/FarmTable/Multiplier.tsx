b12a6270bf8f2629d1b1db01d97b5376efe9a1fe
PoliEx <85815702+PoliEx@users.noreply.github.com>
2021-06-13 13:44:32 +0700

    Update Multiplier.tsx

--- src/views/Farms/components/FarmTable/Multiplier.tsx
+++ src/views/Farms/components/FarmTable/Multiplier.tsx
@@ -33,10 +33,10 @@ const Multiplier: React.FunctionComponent<MultiplierProps> = ({ multiplier }) =>
   const { t } = useTranslation()
   const tooltipContent = (
     <div>
-      {t('The multiplier represents the amount of BECO rewards each farm gets.')}
+      {t('The multiplier represents the amount of POLEX rewards each farm gets.')}
       <br />
       <br />
-      {t('For example, if a 1x farm was getting 1 BECO per block, a 40x farm would be getting 40 BECO per block.')}
+      {t('For example, if a 1x farm was getting 1 POLEX per block, a 40x farm would be getting 40 POLEX per block.')}
     </div>
   )
   const { targetRef, tooltip, tooltipVisible } = useTooltip(tooltipContent, {
