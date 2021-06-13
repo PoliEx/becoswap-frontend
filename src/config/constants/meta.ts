a75901280beabb1e46fbd95754daea018e89a4a1
PoliEx <85815702+PoliEx@users.noreply.github.com>
2021-06-13 11:55:00 +0700

    Update meta.ts

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -1,44 +1,44 @@
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'BecoSwap',
+  title: 'PoliEx Finance',
   description:
-    'The most popular AMM on BSC by user count! Earn BECO through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by BecoSwap), NFTs, and more, on a platform you can trust.',
+    'The most popular AMM on Polygon by user count! Earn POLEX through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PoliExSwap), NFTs, and more, on a platform you can trust.',
   image: 'https://becoswap.com/images/hero.jpg',
 }
 
 export const customMeta: { [key: string]: PageMeta } = {
   '/': {
-    title: 'Home | BecoSwap',
+    title: 'Home | PoliEx',
   },
   '/competition': {
-    title: 'Trading Battle | BecoSwap',
+    title: 'Trading Battle | PoliEx',
   },
   '/prediction': {
-    title: 'Prediction | BecoSwap',
+    title: 'Prediction | PoliEx',
   },
   '/farms': {
-    title: 'Farms | BecoSwap',
+    title: 'Farms | PoliEx',
   },
   '/pools': {
-    title: 'Pools | BecoSwap',
+    title: 'Pools | PoliEx',
   },
   '/lottery': {
-    title: 'Lottery | BecoSwap',
+    title: 'Lottery | PoliEx',
   },
   '/collectibles': {
-    title: 'Collectibles | BecoSwap',
+    title: 'Collectibles | PoliEx',
   },
   '/ifo': {
-    title: 'Initial Farm Offering | BecoSwap',
+    title: 'Initial Farm Offering | PoliEx',
   },
   '/teams': {
-    title: 'Leaderboard | BecoSwap',
+    title: 'Leaderboard | PoliEx',
   },
   '/profile/tasks': {
-    title: 'Task Center | BecoSwap',
+    title: 'Task Center | PoliEx',
   },
   '/profile': {
-    title: 'Your Profile | BecoSwap',
+    title: 'Your Profile | PoliEx',
   },
 }
