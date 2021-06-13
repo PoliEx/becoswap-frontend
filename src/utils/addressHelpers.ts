15492a6e8d4a965d87e6e5ceca30650f8606c4e2
PoliEx <85815702+PoliEx@users.noreply.github.com>
2021-06-13 11:59:19 +0700

    Update addressHelpers.ts

--- src/utils/addressHelpers.ts
+++ src/utils/addressHelpers.ts
@@ -3,13 +3,13 @@ import tokens from 'config/constants/tokens'
 import { Address } from 'config/constants/types'
 
 export const getAddress = (address: Address): string => {
-  const mainNetChainId = 56
+  const mainNetChainId = 137
   const chainId = process.env.REACT_APP_CHAIN_ID
   return address[chainId] ? address[chainId] : address[mainNetChainId]
 }
 
 export const getCakeAddress = () => {
-  return getAddress(tokens.beco.address)
+  return getAddress(tokens.polex.address)
 }
 export const getMasterChefAddress = () => {
   return getAddress(addresses.masterChef)
@@ -18,7 +18,7 @@ export const getMulticallAddress = () => {
   return getAddress(addresses.multiCall)
 }
 export const getWbnbAddress = () => {
-  return getAddress(tokens.wbnb.address)
+  return getAddress(tokens.wmatic.address)
 }
 export const getLotteryAddress = () => {
   return getAddress(addresses.lottery)
