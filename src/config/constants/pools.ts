ad879f67aab9a7f16ae1ce90c8f6b711558dbc2f
PoliEx <85815702+PoliEx@users.noreply.github.com>
2021-06-15 15:55:25 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -8,7 +8,7 @@ const pools: PoolConfig[] = [
     earningToken: tokens.polex,
     contractAddress: {
       80001: '',
-      137: '0x0f733c81ff79be10e722eaabf98d55230fed7504',
+      137: '0xD510754Cce167D9C60d7f311F61e3e7073EEAB3E',
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
