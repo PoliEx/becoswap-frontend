539e3ec1e56a6ae852d274c67fbb4837632883e3
PoliEx <85815702+PoliEx@users.noreply.github.com>
2021-06-17 11:53:32 +0700

    Update FarmCard.tsx

--- src/views/Farms/components/FarmCard/FarmCard.tsx
+++ src/views/Farms/components/FarmCard/FarmCard.tsx
@@ -162,7 +162,7 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, account }
       <ExpandingWrapper expanded={showExpandableSection}>
         <DetailsSection
           removed={removed}
-          bscScanAddress={`https://polygonscan.com/token/${farm.lpAddresses[process.env.REACT_APP_CHAIN_ID]}?a=0x0f733c81ff79be10e722eaabf98d55230fed7504`}
+          bscScanAddress={`https://polygonscan.com/address/${farm.lpAddresses[process.env.REACT_APP_CHAIN_ID]}`}
           infoAddress={`https://info.quickswap.exchange/pair/${lpAddress}`}
           totalValueFormatted={totalValueFormatted}
           lpLabel={lpLabel}
