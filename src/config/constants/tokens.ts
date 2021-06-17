d0702dbce299c91f2a80af59825849570ea1ac9c
PoliEx <85815702+PoliEx@users.noreply.github.com>
2021-06-17 20:41:16 +0700

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -12,15 +12,6 @@ const tokens = {
     decimals: 18,
     projectLink: 'https://polyswap.netlify.app/',
   },
-  btcb: {
-    symbol: 'BTCB',
-    address: {
-      137: '0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c',
-      80001: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
-    },
-    decimals: 18,
-    projectLink: 'https://bitcoin.org/',
-  },
   wmatic: {
     symbol: 'WMATIC',
     address: {
@@ -46,7 +37,7 @@ const tokens = {
       137: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
       80001: '',
     },
-    decimals: 18,
+    decimals: 6,
     projectLink: 'https://www.centre.io/usdc',
   },
  
@@ -56,22 +47,9 @@ const tokens = {
       137: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
       80001: '',
     },
-    decimals: 18,
+    decimals: 6,
     projectLink: 'https://tether.to/',
-  },
-
- 
-  dai: {
-    symbol: 'DAI',
-    address: {
-      137: '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3',
-      80001: '',
-    },
-    decimals: 18,
-    projectLink: 'http://www.makerdao.com/',
-  },
-
-  
+  }, 
 }
 
 export default tokens
