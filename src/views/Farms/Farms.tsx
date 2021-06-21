419202217dcac160cde100b24cebf73dd306c53c
PoliEx <85815702+PoliEx@users.noreply.github.com>
2021-06-21 09:09:11 +0700

    Update Farms.tsx

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -269,7 +269,7 @@ const Farms: React.FC = () => {
     const { token, quoteToken } = farm
     const tokenAddress = token.address
     const quoteTokenAddress = quoteToken.address
-    const lpLabel = farm.lpSymbol && farm.lpSymbol.split(' ')[0].toUpperCase().replace('POLEXSWAP', '')
+    const lpLabel = farm.lpSymbol && farm.lpSymbol.split(' ')[0].toUpperCase().replace('QUICKSWAP', '')
 
     const row: RowProps = {
       apr: {
